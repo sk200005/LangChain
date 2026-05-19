e44fca8db0c6479ecfccb02fd7caee59b618bd9f
sk200005 <swayamkorde2005@gmail.com>
2026-05-19 23:11:12 +0530

    Updated some stuff

---
+++ LLMs/llm_Demo.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+result = llm.invoke("What is the capital of India?")
+
+print(result.content)
